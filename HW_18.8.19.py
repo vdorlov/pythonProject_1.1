5bd5111e9e67e65648c7e742147d0f863f4d8d4e
vdorlov <orlov.vd.mpf4@gmail.com>
2023-07-03 08:53:31 +0300

    Initial commit

--- HW_18.8.19.py
+++ HW_18.8.19.py
@@ -1,2 +1,13 @@
-tik = int(input())
-age =
+tickets = int(input('Введите количество приобретаемых билетов: '))
+counter = 0
+for i in range(tickets):
+    age = int(input('Введите возраст посетителя: '))
+    if age <= 17:
+        counter += 0
+    elif 18 <= age <= 24:
+        counter += 990
+    elif age >= 25:
+        counter += 1390
+if tickets >= 3:
+    counter = counter * 0.90
+print('Общая сумма к оплате:', int(counter))
